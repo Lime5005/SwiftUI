a3be59567e66672d6d63d7b15359f1c21cbe7ae4
Lime5005 <zlp5005@gmail.com>
2020-12-20 00:35:08 +0100

    Initial Commit

---
+++ Wesplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wesplit
+//
+//  Created by Liping Mechling on 20/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
