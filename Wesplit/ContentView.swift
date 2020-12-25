10a968fcef931ef5f2db48b58dba47ca52e296da
Lime5005 <zlp5005@gmail.com>
2020-12-25 19:40:03 +0100

    conditional modifiers: ternary operator

--- Wesplit/ContentView.swift
+++ Wesplit/ContentView.swift
@@ -66,6 +66,7 @@ struct ContentView: View {
                 
                 Section(header: Text("Total amount to pay:")) {
                     Text("\(totalToPay, specifier: "%.2f")")
+                        .foregroundColor(tipPercentage == 4 ? .red : .black)
                 }
                 
                 Section(header: Text("Amount per person:")) {
